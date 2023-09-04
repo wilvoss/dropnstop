c9baf8e4ff53a6459129f43389d27977c9075064
wilvoss <wilv@allovus.com>
2023-09-04 14:18:04 -0700

    dnd 3.0.001 - updated donate button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // the cache version gets updated every time there is a new deployment
-const CACHE_VERSION = 3.0;
+const CACHE_VERSION = '3.0.001';
 const CURRENT_CACHE = `main-${CACHE_VERSION}`;
 
 // prettier-ignore
