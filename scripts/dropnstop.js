b6d90571c51d8dedde0640ae537082bf56798ade
wilvoss <wilv@allovus.com>
2025-06-05 16:56:20 -0700

    3.0.033 - duh

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.032',
+    version: '3.0.033',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
