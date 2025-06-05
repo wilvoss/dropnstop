0a959c4959014d1b18ba3a28b25802e0c300684e
wilvoss <wilv@allovus.com>
2025-06-05 10:09:43 -0700

    3.0.015 - full sw.js removal

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.014',
+    version: '3.0.015',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
