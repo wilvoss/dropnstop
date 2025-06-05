19b88e0d8063be9c68e9ed7fabee5b28e8ab7bcd
wilvoss <wilv@allovus.com>
2025-06-05 16:54:41 -0700

    3.0.032 - puck buttons opacity

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.031',
+    version: '3.0.032',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
