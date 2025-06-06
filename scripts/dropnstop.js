00cc1bb2eaf86048faa268b7d63ed463606697dc
wilvoss <wilv@allovus.com>
2025-06-06 10:18:59 -0700

    3.0.042 - value on success fixed

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.041',
+    version: '3.0.042',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
