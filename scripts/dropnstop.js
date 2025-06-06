f8db742d91e0695e9dcde67566fbc4368181c19a
wilvoss <wilv@allovus.com>
2025-06-06 08:39:40 -0700

    3.0.040 - ui tweaks

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.039',
+    version: '3.0.040',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
