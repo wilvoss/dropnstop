7ab0b3fa1deda255ef1f0e012b9166f085a37cdd
wilvoss <wilv@allovus.com>
2025-06-05 18:10:14 -0700

    3.0.038 - end game tweaks

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.037',
+    version: '3.0.038',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
