3c155e47a72bbd20bd5818a602f128f857c92d57
wilvoss <wilv@allovus.com>
2025-06-05 17:21:01 -0700

    3.0.035 - end game ui fixes

--- scripts/dropnstop.js
+++ scripts/dropnstop.js
@@ -12,7 +12,7 @@ Vue.config.ignoredElements = ['app', 'page', 'navbar', 'settings', 'splash', 'sp
 var app = new Vue({
   el: '#app',
   data: {
-    version: '3.0.034',
+    version: '3.0.035',
     displayMode: 'browser tab',
     isDropping: false,
     isStopped: true,
